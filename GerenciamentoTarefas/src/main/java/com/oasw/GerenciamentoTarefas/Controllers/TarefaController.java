ebe69123c954dfcbe346636f3ba249a8d96f5bd6
OtavioAugusto <santostatogusto@gmail.com>
2026-03-22 20:17:08 -0300

    controller criado

---
+++ GerenciamentoTarefas/src/main/java/com/oasw/GerenciamentoTarefas/Controllers/TarefaController.java
@@ -0,0 +1,45 @@
+package com.oasw.GerenciamentoTarefas.Controllers;
+
+import com.oasw.GerenciamentoTarefas.Models.TarefaModel;
+import com.oasw.GerenciamentoTarefas.Services.TarefaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/tarefas")
+public class TarefaController {
+
+    @Autowired
+    TarefaService tarefaService;
+
+    @PostMapping
+    public ResponseEntity<TarefaModel> create(TarefaModel tarefaModel){
+        TarefaModel request = tarefaService.create(tarefaModel);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(tarefaModel.getId()).toUri();
+        return ResponseEntity.created(uri).body(request);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TarefaModel>> getAll(){
+        List<TarefaModel> request = tarefaService.getAll();
+        return ResponseEntity.ok().body(request);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TarefaModel> getById(@PathVariable Long id){
+        TarefaModel request = tarefaService.getById(id);
+        return ResponseEntity.ok().body(request);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteById(@PathVariable Long id){
+        tarefaService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
