703ea50f60b36e32196b3b12cd8fce72ffc50440
OtavioAugusto <santostatogusto@gmail.com>
2026-03-22 19:58:17 -0300

    model criado

---
+++ GerenciamentoTarefas/src/main/java/com/oasw/GerenciamentoTarefas/Models/TarefaModel.java
@@ -0,0 +1,53 @@
+package com.oasw.GerenciamentoTarefas.Models;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_tarefa")
+public class TarefaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String descricao;
+    private LocalDate dataVencimento;
+    @Column(name = "Status")
+    private Boolean concluido;
+
+    public TarefaModel() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDataVencimento() {
+        return dataVencimento;
+    }
+
+    public void setDataVencimento(LocalDate dataVencimento) {
+        this.dataVencimento = dataVencimento;
+    }
+
+    public Boolean getConcluido() {
+        return concluido;
+    }
+
+    public void setConcluido(Boolean concluido) {
+        this.concluido = concluido;
+    }
+}
---
+++ GerenciamentoTarefas/src/test/java/com/oasw/GerenciamentoTarefas/GerenciamentoTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oasw.GerenciamentoTarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
